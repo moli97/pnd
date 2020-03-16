fc3d0e23216888426952a6c6a99b5cd23edecfe6
moli <moli@hulai.com>
2020-03-16 21:09:10 +0800

    yml

--- web/src/main/java/site/bitinit/pnd/web/filter/LoginFilter.java
+++ web/src/main/java/site/bitinit/pnd/web/filter/LoginFilter.java
@@ -2,16 +2,12 @@ package site.bitinit.pnd.web.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import site.bitinit.pnd.web.config.StorageHandler;
-import site.bitinit.pnd.web.util.ResponseUtil;
-import site.bitinit.pnd.web.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
@@ -27,14 +23,15 @@ public class LoginFilter implements Filter {
 
 		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 		String accessToken = httpRequest.getHeader("accessToken");
-		if (StringUtils.isBlank(accessToken) || StringUtils.isBlank(storageHandler.get(accessToken))) {
-			storageHandler.clearInvalid();
-			log.error(accessToken);
-			HttpServletResponse response = (HttpServletResponse) servletResponse;
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			ResponseUtil.out(response, ResponseUtil.resultMap("无访问权限"));
-			return;
-		}
-		filterChain.doFilter(servletRequest, servletResponse);
+		/**if (StringUtils.isBlank(accessToken) || StringUtils.isBlank(storageHandler.get(accessToken))) {
+		 storageHandler.clearInvalid();
+		 log.error(accessToken);
+		 HttpServletResponse response = (HttpServletResponse) servletResponse;
+		 response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		 ResponseUtil.out(response, ResponseUtil.resultMap("无访问权限"));
+		 return;
+		 }*/
+		servletRequest.getRequestDispatcher("/v1/login").forward(servletRequest, servletResponse);
+		//filterChain.doFilter(servletRequest, servletResponse);
 	}
 }
